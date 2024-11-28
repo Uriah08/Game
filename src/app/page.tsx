c2d434ae3d43a8e23bdbd91fecf1a0b960bdf813
Uriah08 <lorenz08.flores@gmail.com>
2024-11-28 22:33:55 +0800

    Unfinished Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import About from "./components/About";
 import Hero from "./components/Hero";
+import NavBar from "./components/NavBar";
 
 // eslint-disable-next-line import/no-anonymous-default-export, react/display-name
 export default function () {
   return (
     <div className="relative min-h-screen w-screen overflow-x-hidden">
+      <NavBar/>
       <Hero/>
       <About/>
     </div>
